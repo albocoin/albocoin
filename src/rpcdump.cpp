ca35849113ca27c474b8a9514c1801c17c72e1ee
albi300 <albi300@gmail.com>
2014-03-12 16:41:41 -0400

    Update rpcdump.cpp

--- src/rpcdump.cpp
+++ src/rpcdump.cpp
@@ -75,8 +75,8 @@ Value dumpprivkey(const Array& params, bool fHelp)
 {
     if (fHelp || params.size() != 1)
         throw runtime_error(
-        "dumpprivkey <dogecoinaddress>\n"
-        "Reveals the private key corresponding to <dogecoinaddress>.");
+        "dumpprivkey <albocoinaddress>\n"
+        "Reveals the private key corresponding to <albocoinaddress>.");
 
     string strAddress = params[0].get_str();
     CBitcoinAddress address;
